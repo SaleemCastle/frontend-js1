21e45beccedb63be57dc51f0abf2a83c42adc40a
Saleem Castle <saleemcastle94@gmail.com>
2023-10-07 11:42:30 -0500

    refactor

--- game.js
+++ game.js
@@ -1,47 +1,49 @@
 function computerPlay() {
-    const choices = ["Rock", "Paper", "Scissors"];
-	const size = choices.length;
-    const getRandomIndex = Math.floor(Math.random() * size);
-    return choices[getRandomIndex];
+    const options = ["Rock", "Paper", "Scissors"];
+	const size = options.length;
+    const optionsIndex = Math.floor(Math.random() * size)
+    return options[optionsIndex];
 }
 
-function playRound(playerSelection, computerSelection) {
-	playerSelection = playerSelection.toLowerCase(); 
+function playRound(userChoice, computerChoice) {
+	userChoice = userChoice.trim().toLowerCase(); 
 	
 	if (
-		(playerSelection === "rock" && computerSelection === "Scissors") ||
-		(playerSelection === "paper" && computerSelection === "Rock") ||
-		(playerSelection === "scissors" && computerSelection === "Paper")
+		(userChoice === "rock" && computerChoice === "Scissors") ||
+		(userChoice === "paper" && computerChoice === "Rock") ||
+		(userChoice === "scissors" && computerChoice === "Paper")
 	) {
-		playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.substring(1);
-		return `You Win! ${playerSelection} beats ${computerSelection}`;
+		return `You Win! ${userChoice} beats ${computerChoice}`;
 	} else if (
-		(playerSelection === "rock" && computerSelection === "Paper") ||
-		(playerSelection === "paper" && computerSelection === "Scissors") ||
-		(playerSelection === "scissors" && computerSelection === "Rock")
+		(userChoice === "rock" && computerChoice === "Paper") ||
+		(userChoice === "paper" && computerChoice === "Scissors") ||
+		(userChoice === "scissors" && computerChoice === "Rock")
 	) {
-		playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.substring(1);
-		return `You Lose! ${computerSelection} beats ${playerSelection}`;
+		return `You Lose! ${computerChoice} beats ${userChoice}`;
 	} else {
-		playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.substring(1);
-		return "It's a Tie! Both chose " + playerSelection;
+		return "It's a Tie! Both chose " + userChoice;
 	}
 }
 
-function getUserChoice() {
-	let userChoice = "";
+function getUserChoice(round) {
+	let userOption = "";
 	while (true) {
-		userChoice = prompt("Enter your choice: Rock, Paper, or Scissors");
+		userOption = prompt(`Round ${round} - Enter your choice: Rock, Paper, or Scissors`);
 		
-		if (userChoice === null) {
-			alert("You cannot exit the game prematurely. Please make a choice.");
-			continue;
-		}
+		// Check if the user pressed cancel
+        if (userOption === null) {
+            const leaveGame = confirm("Are you sure you want to leave the game?");
+            if (leaveGame) {
+              alert("Thanks for playing! See you next time.");
+              return null;
+            }
+            continue;
+        }
 		
-		userChoice = userChoice.toLowerCase().trim();
+		userOption = userOption.toLowerCase().trim();
 		
-		if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
-			return userChoice;
+		if (userOption === "rock" || userOption === "paper" || userOption === "scissors") {
+			return userOption;
 		} else {
 			alert("Invalid choice. Please enter Rock, Paper, or Scissors.");
 		}
@@ -52,11 +54,12 @@ function game() {
 	let playerScore = 0;
 	let computerScore = 0;
 
-	for (let i = 0; i < 5; i++) {
-		const playerSelection = getUserChoice();
-		const computerSelection = computerPlay();
+	for (let index = 0; index < 5; index++) {
+        const round = index + 1
+		const playerSelection = getUserChoice(round);
+		const computerChoice = computerPlay();
 
-		const result = playRound(playerSelection, computerSelection);
+		const result = playRound(playerSelection, computerChoice);
 		console.log(result);
 
 		if (result.startsWith("You Win!")) {
@@ -75,4 +78,16 @@ function game() {
 	}
 }
 
-game();
+// Function to start the game
+function startGame() {
+    const startPrompt = "Do you want to start a game of Rock, Paper, Scissors? (Yes/No)";
+    const userResponse = prompt(startPrompt);
+  
+    if (userResponse !== null && userResponse.toLowerCase() === "yes") {
+      game();
+    } else {
+      alert("Okay, maybe next time!");
+    }
+  }
+
+startGame();
